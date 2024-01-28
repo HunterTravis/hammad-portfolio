4912b554c5551485562a58e0ec2e09188fa1afd9
Hammad Ahmed <62638943+HunterTravis@users.noreply.github.com>
2024-01-28 22:46:09 +0500

    initial setup of components

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: PortfolioPageComponent,
+        title : "Hammad Ahmed"
+    },
+];
---
+++ src/app/portfolio-page/portfolio-page.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-page',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      portfolio-page works!
+    </p>
+  `,
+  styleUrl: './portfolio-page.component.scss'
+})
+export class PortfolioPageComponent {
+
+}
